f4d47541175583d3c7b3b1b43346fad4f6b6d7d3
IanMensitieri <141031523+IanMensitieri@users.noreply.github.com>
2023-12-03 23:49:27 -0300

    Update q1.go

--- q1/q1.go
+++ q1/q1.go
@@ -13,8 +13,6 @@ package q1
 //A função deve retornar um valor booleano, indicando se é possível ou não dividir a melancia da forma desejada. Se o peso
 //da melancia for menor ou igual a 0, a função deve retornar um erro.
 
-package main
-
 import (
 	"errors"
 	"fmt"
